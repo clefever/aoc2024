4281fa04961a52ef44a03da231b6d41d1b3284be
Chris Lefever <linuxcl4@gmail.com>
2024-12-02 02:06:53 -0500

    Refactoring for day 1

--- day01.py
+++ day01.py
@@ -8,12 +8,7 @@ def part1(input):
     """
     input[0].sort()
     input[1].sort()
-
-    sum = 0
-    for i in range(len(input[0])):
-        sum += abs(input[0][i] - input[1][i])
-
-    return sum
+    return sum(abs(input[0][i] - input[1][i]) for i in range(len(input[0])))
 
 
 def part2(input):
@@ -21,17 +16,12 @@ def part2(input):
     >>> part2([[3, 4, 2, 1, 3, 3], [4, 3, 5, 3, 9, 3]])
     31
     """
-    sum = 0
-    for i in range(len(input[0])):
-        similarity = input[1].count(input[0][i])
-        sum += input[0][i] * similarity
-
-    return sum
+    return sum(input[0][i] * input[1].count(input[0][i]) for i in range(len(input[0])))
 
 
 def main():
     puzzle_input = adventofcode.read_input(1)
-    input_nums = [list(l) for l in zip(*[[int(n) for n in num.split()] for num in puzzle_input])]
+    input_nums = list(map(list, zip(*[[int(num) for num in line.split()] for line in puzzle_input])))
     adventofcode.answer(1, 2580760, part1(input_nums))
     adventofcode.answer(2, 25358365, part2(input_nums))
 
